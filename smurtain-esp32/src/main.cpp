9fcc96198aff26459a2e11829199ec900424b6fb
NewBieCoderXD <fruity5555@gmail.com>
2024-11-18 16:08:27 +0700

    support UART

--- smurtain-esp32/src/main.cpp
+++ smurtain-esp32/src/main.cpp
@@ -7,7 +7,7 @@
 #define TEMP_PIN 4
 #define ULTRASONIC_TRIG_PIN 27
 #define ULTRASONIC_ECHO_PIN 27
-#define TX_PIN 16
+#define TX_PIN 18
 #define RX_PIN 19
 
 EspSoftwareSerial::UART gatewaySerial;
@@ -19,13 +19,20 @@ void setup()
   while (!Serial)
     ;
 
-  gatewaySerial.begin(115200, EspSoftwareSerial::SWSERIAL_7O1, RX_PIN, TX_PIN);
+  gatewaySerial.begin(9600, EspSoftwareSerial::SWSERIAL_8O1, RX_PIN, TX_PIN);
 
-  while (!gatewaySerial)
-    ;
+  while (!gatewaySerial){
+    Serial.println("error");
+  }
+
+  Serial.println("started");
 }
 
 void loop()
 {
-  gatewaySerial.println("gg");
+  gatewaySerial.println("something");
+  while (gatewaySerial.available())
+  {
+    Serial.print((char)gatewaySerial.read());
+  }
 }
