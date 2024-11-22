67b7c22198d5a08943c3bcb70d02f9909d0351a6
Oatkrub <sirawit.v@gmail.com>
2024-11-22 15:49:39 +0700

    node cache

--- frontend/service/broker.service.ts
+++ frontend/service/broker.service.ts
@@ -1,13 +1,13 @@
 import { isPlatformBrowser } from '@angular/common';
-import {
-  Inject,
-  Injectable,
-  InjectionToken,
-  NgModule,
-  PLATFORM_ID,
-} from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Inject, Injectable, InjectionToken, PLATFORM_ID } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 import mqtt from 'mqtt-browser';
+import NodeCache from 'node-cache'; // Import NodeCache
+
+export interface DataWithTime {
+  Time: number;
+  Data: number;
+}
 
 const REQUEST_TOPIC = 'request';
 const TEMPERATURE_TOPIC = 'temperature';
@@ -24,22 +24,20 @@ export const MQTT_OPTIONS: mqtt.IClientOptions = {
   providedIn: 'root',
 })
 export class BrokerService {
+  private cache: NodeCache = new NodeCache(); // Use NodeCache instead of Map
+  public cache$ = new BehaviorSubject<DataWithTime[]>([]);
   public client: mqtt.MqttClient | undefined;
   temperature: Subject<number> = new Subject();
   light: Subject<number> = new Subject();
   humidity: Subject<number> = new Subject();
-  // humidity = new Subject();
 
   constructor(@Inject(PLATFORM_ID) private platformId: InjectionToken<Object>) {
     if (isPlatformBrowser(platformId)) {
+      this.cache = new NodeCache(); // Initialize NodeCache
       this.client = mqtt.connect(MQTT_OPTIONS);
+
       this.client.on('connect', () => {
         this.client?.subscribe(TEMPERATURE_TOPIC, (err) => {
-          if (err) {
-            console.log('error from connecting mqtt', err);
-          }
-        });
-        this.client?.subscribe(LIGHT_TOPIC, (err) => {
           if (err) {
             console.log('error from connecting mqtt', err);
           }
@@ -58,17 +56,54 @@ export class BrokerService {
 
       this.client.on('message', (topic, message) => {
         if (topic == TEMPERATURE_TOPIC) {
-          this.temperature.next(parseFloat(message.toString()))
-          // this.light.next(parseFloat(message.toString()))
+          this.temperature.next(parseFloat(message.toString()));
+          const time = Date.now();
+          this.set(topic, {
+            Time: time,
+            Data: parseFloat(message.toString()),
+          });
+          console.log('cache', this.cache);
+          console.log('behavior', this.cache$);
         }
         if (topic == LIGHT_TOPIC) {
-          this.light.next(parseFloat(message.toString()))
+          this.light.next(parseFloat(message.toString()));
+          const time = Date.now();
+          this.set(topic, {
+            Time: time,
+            Data: parseFloat(message.toString()),
+          });
+          console.log('cache', this.cache);
+          console.log('behavior', this.cache$);
         }
         if (topic == HUMIDITY_TOPIC) {
-          this.humidity.next(parseFloat(message.toString()))
+          this.humidity.next(parseFloat(message.toString()));
+          const time = Date.now();
+          this.set(topic, {
+            Time: time,
+            Data: parseFloat(message.toString()),
+          });
+          console.log('cache', this.cache);
+          console.log('behavior', this.cache$);
         }
       });
     }
   }
- 
+
+  set(key: string, data: DataWithTime) {
+    let existingData = this.cache.get(key) as DataWithTime[]; // Get existing data from cache
+
+    if (existingData) {
+      existingData.push(data); // Push new data if exists
+    } else {
+      existingData = [data]; // Create new array if no data exists
+    }
+
+    this.cache.set(key, existingData); // Set the updated data back into cache
+
+    // Flatten cache values and update BehaviorSubject
+    const allCacheData = Array.from(this.cache.keys()).flatMap(
+      (key) => this.cache.get(key) as DataWithTime[]
+    );
+    this.cache$.next(allCacheData);
+  }
 }
