fe8f9829ce8e811cde997d4a213956e532a23326
Sirawit Viriyaphisathan <sirawit@SorawitWork.local>
2024-11-28 11:46:01 +0700

    Publish mongodb data 5 mins max

--- backend/go.mod
+++ backend/go.mod
@@ -8,6 +8,7 @@ require (
 	cloud.google.com/go/auth v0.10.2 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.5 // indirect
 	cloud.google.com/go/compute/metadata v0.5.2 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -17,8 +18,15 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.0 // indirect
-	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
@@ -41,6 +49,7 @@ require (
 )
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/xid v1.4.0 // indirect
---
+++ backend/src/pkg/mongo_service/mongo_service.go
@@ -0,0 +1,121 @@
+package mongo_service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/mochi-mqtt/server/v2"
+	"github.com/mochi-mqtt/server/v2/packets"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoClient *mongo.Client
+
+func ConnectToMongoDB() {
+
+	mongoURI := "mongodb+srv://sirawitv:Smurtain@smurtain.fjgsc.mongodb.net/"
+	clientOptions := options.Client().ApplyURI(mongoURI)
+
+	var err error
+	MongoClient, err = mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	// Check connection
+	err = MongoClient.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal("MongoDB connection ping failed:", err)
+	}
+
+	log.Println("Connected to MongoDB")
+}
+
+func SubscribeToTopics(server *mqtt.Server) {
+	err := server.Subscribe("status/#", 1, func(cl *mqtt.Client, sub packets.Subscription, pk packets.Packet) {
+		server.Log.Info("received")
+		message := string(pk.Payload)
+		topic := pk.TopicName
+		fmt.Println("Message received:", message, topic)
+
+		insertDataToMongo(topic, message)
+
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func insertDataToMongo(topic string, message string) {
+	collection := MongoClient.Database("smurtain").Collection("smurtain")
+
+	// Create a document to insert
+	doc := map[string]interface{}{
+		"topic":     topic,
+		"message":   message,
+		"timestamp": time.Now(),
+	}
+
+	// Insert the document
+	_, err := collection.InsertOne(context.Background(), doc)
+	if err != nil {
+		log.Printf("Failed to insert message into MongoDB: %v\n", err)
+	} else {
+		log.Println("Message successfully inserted into MongoDB")
+	}
+}
+func GetDataMongoUsingMQTT(server *mqtt.Server) {
+	requestTopic := "data/record/request"
+	responseTopic := "data/record/response"
+
+	err := server.Subscribe(requestTopic, 1, func(cl *mqtt.Client, sub packets.Subscription, pk packets.Packet) {
+		server.Log.Info("Data request received")
+
+		// Parse any payload data if required
+		fiveMinutesAgo := time.Now().Add(-5 * time.Minute)
+
+		collection := MongoClient.Database("smurtain").Collection("smurtain")
+		filter := bson.M{
+			"timestamp": bson.M{
+				"$gte": fiveMinutesAgo,
+			},
+		}
+
+		var results []bson.D
+		cursor, err := collection.Find(context.TODO(), filter)
+		if err != nil {
+			log.Printf("Error querying MongoDB: %v\n", err)
+			return
+		}
+
+		if err = cursor.All(context.TODO(), &results); err != nil {
+			log.Printf("Error parsing MongoDB cursor: %v\n", err)
+			return
+		}
+
+		// Serialize results (e.g., JSON format)
+		responsePayload, err := json.Marshal(results)
+		fmt.Println(string(responsePayload))
+		if err != nil {
+			log.Printf("Error marshalling results: %v\n", err)
+			return
+		}
+
+		// Publish results to the response topic
+		err = server.Publish(responseTopic, responsePayload, false, 1)
+		if err != nil {
+			log.Printf("Failed to publish response: %v\n", err)
+		} else {
+			log.Println("Query results successfully published to response topic")
+		}
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to subscribe to request topic: %v\n", err)
+	}
+}
